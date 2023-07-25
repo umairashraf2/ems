e1d1715c057a36bb899f26645ce7311f53c51d94
Umair Ashraf <umair.dco@gmail.com>
2023-07-25 15:13:34 +0500

    new

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,95 +1,102 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function Home() {
-  const [adminCount, setAdminCount] = useState()
-  const [employeeCount, setEmployeeCount] = useState()
-  const [salary, setSalary] = useState()
-  const [data, setData] = useState()
+  const [adminCount, setAdminCount] = useState();
+  const [employeeCount, setEmployeeCount] = useState();
+  const [salary, setSalary] = useState();
+  const [data, setData] = useState();
 
   useEffect(() => {
-    axios.get('http://localhost:8081/getUsers')
-    .then(res => {
-      setData(res.data.Result)
-    }).catch(err => console.log(err));
+    axios
+      .get("http://13.48.91.251:8081/getUsers")
+      .then((res) => {
+        setData(res.data.Result);
+      })
+      .catch((err) => console.log(err));
 
-    axios.get('http://localhost:8081/adminCount')
-		.then(res => {
-			setAdminCount(res.data[0].admin)
-		}).catch(err => console.log(err));
+    axios
+      .get("http://13.48.91.251:8081/adminCount")
+      .then((res) => {
+        setAdminCount(res.data[0].admin);
+      })
+      .catch((err) => console.log(err));
 
-    axios.get('http://localhost:8081/employeeCount')
-		.then(res => {
-			setEmployeeCount(res.data[0].employee)
-		}).catch(err => console.log(err));
+    axios
+      .get("http://13.48.91.251:8081/employeeCount")
+      .then((res) => {
+        setEmployeeCount(res.data[0].employee);
+      })
+      .catch((err) => console.log(err));
 
-    axios.get('http://localhost:8081/salary')
-		.then(res => {
-			setSalary(res.data[0].sumOfSalary)
-		}).catch(err => console.log(err));
-
-  } , [])
+    axios
+      .get("http://13.48.91.251:8081/salary")
+      .then((res) => {
+        setSalary(res.data[0].sumOfSalary);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
-		<div>
-			<div className="p-3 d-flex justify-content-around mt-3">
-				<div className="px-3 pt-2 pb-3 border shadow-sm w-25">
-					<div className="text-center pb-1">
-						<h4>Admin</h4>
-					</div>
-					<hr />
-					<div className="">
-						<h5>Total: {adminCount}</h5>
-					</div>
-				</div>
-				<div className="px-3 pt-2 pb-3 border shadow-sm w-25">
-					<div className="text-center pb-1">
-						<h4>Employee</h4>
-					</div>
-					<hr />
-					<div className="">
-						<h5>Total: {employeeCount}</h5>
-					</div>
-				</div>
-				<div className="px-3 pt-2 pb-3 border shadow-sm w-25">
-					<div className="text-center pb-1">
-						<h4>Salary</h4>
-					</div>
-					<hr />
-					<div className="">
-						<h5>Total: {salary}</h5>
-					</div>
-				</div>
-			</div>
+    <div>
+      <div className="p-3 d-flex justify-content-around mt-3">
+        <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
+          <div className="text-center pb-1">
+            <h4>Admin</h4>
+          </div>
+          <hr />
+          <div className="">
+            <h5>Total: {adminCount}</h5>
+          </div>
+        </div>
+        <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
+          <div className="text-center pb-1">
+            <h4>Employee</h4>
+          </div>
+          <hr />
+          <div className="">
+            <h5>Total: {employeeCount}</h5>
+          </div>
+        </div>
+        <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
+          <div className="text-center pb-1">
+            <h4>Salary</h4>
+          </div>
+          <hr />
+          <div className="">
+            <h5>Total: {salary}</h5>
+          </div>
+        </div>
+      </div>
 
-			{/* List of admin  */}
-			<div className="mt-4 px-5 pt-3">
-				<h3>List of Admins</h3>
-				<table className="table">
-					<thead>
-						<tr>
-							<th>ID</th>
-							<th>Name</th>
-							<th>Email</th>
-							<th>Role</th>
-						</tr>
-					</thead>
-					<tbody>
-						{data &&
-							data.map((admin, index) => {
-								return (
-									<tr key={index}>
+      {/* List of admin  */}
+      <div className="mt-4 px-5 pt-3">
+        <h3>List of Admins</h3>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data &&
+              data.map((admin, index) => {
+                return (
+                  <tr key={index}>
                     <td>{admin.id}</td>
-										<td>{admin.username}</td>
-										<td>{admin.email}</td>
-										<td>{admin.role}</td>
-									</tr>
-								);
-							})}
-					</tbody>
-				</table>
-			</div>
-		</div>
+                    <td>{admin.username}</td>
+                    <td>{admin.email}</td>
+                    <td>{admin.role}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
-export default Home
+export default Home;
